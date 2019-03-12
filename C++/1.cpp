983bdbd7cfe96e85fd0eeba253db72b11399af5d
panghongye <cc_@live.com>
2019-03-12 17:01:52 +0800

    0

--- C++/1.cpp
+++ C++/1.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <ctime>
-
+#include <math.h>
 using namespace std;
-
-void fn()
-{
-    // 基于当前系统的当前日期/时间
-    time_t now = time(0);
-    // 把 now 转换为字符串形式
-    char *dt = ctime(&now);
-    cout << "本地日期和时间：" << dt << endl;
-    fn();
-}
-
 int main()
 {
-    fn();
+    cout << "请输入半菱形高度n" << endl;
+    int n = 10;
+    // cin >> n;
+    for (int i = 0; i < 2 * n + 1; i++)
+    {
+        for (int j = 0; j < 2 * n + 1; j++)
+        {
+            if (abs(i - n) + abs(j - n) == n)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
 }
