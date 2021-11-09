29da8af1a5caa09b081fa00c46317b67b5aced24
ex_hongye.pang <ex_hongye.pang@tcl.com>
2021-11-09 10:06:24 +0800

    0

--- Rust/char/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#[macro_use]
-extern crate lazy_static;
-
-pub mod client;
-pub mod error;
-pub mod hub;
-pub mod model;
-pub mod proto;
-pub mod server;
--- Rust/char/src/main.rs
+++
@@ -1,9 +0,0 @@
-use rusty_chat::server::Server;
-#[tokio::main]
-pub async fn main() {
-    env_logger::init();
-    let port = 8080;
-    let server = Server::new(port);
-    format!("port is {}", port);
-    server.run().await;
-}
