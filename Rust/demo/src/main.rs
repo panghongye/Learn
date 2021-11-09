9e60f617de1d4ac646c80b43b7fda851be64c976
ex_hongye.pang <ex_hongye.pang@tcl.com>
2021-11-09 12:24:22 +0800

    0

--- Rust/demo/src/main.rs
+++ Rust/demo/src/main.rs
@@ -3,12 +3,4 @@ fn main() {
     //  rusty-chat::main();
     // println!(rusty-chat);
 
-    let a = 10;
-    println!("{}", a);
-
-    let mut vector = vec![1, 2, 4, 8];
-    vector.push(16);
-    vector.push(32);
-    vector.push(64);
-    println!("{:?}", vector);
 }
