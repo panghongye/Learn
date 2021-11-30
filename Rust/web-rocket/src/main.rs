b0c570f0f306467fb7c051c5482f652c51014d2a
ex_hongye.pang <ex_hongye.pang@tcl.com>
2021-11-30 18:55:27 +0800

    0

--- Rust/web-rocket/src/main.rs
+++ Rust/web-rocket/src/main.rs
@@ -1,17 +1,30 @@
 #[macro_use]
 extern crate rocket;
+use rocket::http::CookieJar;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/a")]
-fn a() -> String {
-    "aaaaaaa".to_string()
+#[get("/<name>")]
+fn name(name: &str) -> String {
+    format!("route: /{}!", name)
+}
+
+#[get("/query?<name>")]
+fn query(name: &str) -> String {
+    format!("query: name= {}!", name)
+}
+
+#[get("/cookie")]
+fn cookie(cookies: &CookieJar<'_>) -> Option<String> {
+    cookies
+        .get("message")
+        .map(|crumb| format!("Message: {}", crumb.value()))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, a])
+    rocket::build().mount("/", routes![index, name, cookie, query])
 }
