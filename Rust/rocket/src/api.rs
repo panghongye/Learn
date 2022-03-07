da520a34515dba4650eb817a330ffcea9e925351
ex_hongye.pang <ex_hongye.pang@tcl.com>
2022-03-07 11:20:57 +0800

    0

--- Rust/rocket/src/api.rs
+++ Rust/rocket/src/api.rs
@@ -7,11 +7,13 @@ use sqlx::mysql::MySqlPool;
 use std::env;
 
 pub fn stage() -> AdHoc {
-    rocket::fairing::AdHoc::on_ignite("API", |rocket| async { rocket.mount("/", routes![]) })
+    rocket::fairing::AdHoc::on_ignite("API", |rocket| async {
+        rocket.mount("/", routes![add_data])
+    })
 }
-
-pub async fn add_data()  {
+#[get("/add_data")]
+pub async fn add_data() -> anyhow::Result<()> {
     let pool = MySqlPool::connect(&env::var("DATABASE_URL")?).await?;
-
     sqlx::query("DELETE FROM table").execute(&pool).await?;
+    Ok(())
 }
