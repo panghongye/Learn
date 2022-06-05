14bcf88b7d4c13596c1e2dba42f3a77ffeb71075
cc <cc_@live.com>
2022-06-05 21:56:48 +0800

    0

--- Rust/salvo/src/main.rs
+++ Rust/salvo/src/main.rs
@@ -6,32 +6,39 @@ extern crate lazy_static;
 
 use rbatis::crud::CRUD;
 use rbatis::rbatis::Rbatis;
-use salvo::{hyper::header, prelude::*};
+use salvo::prelude::*;
 use serde::{Deserialize, Serialize};
+use serde_json;
 
 lazy_static! {
   pub static ref RB:Rbatis = Rbatis::new(); // Rbatis是线程、协程安全的，运行时的方法是Send+Sync，无需担心线程竞争
 }
 
-
 #[crud_table]
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct TabTodo {
-    pub id: i32,
+    pub id: Option<i32>,
     pub body: Option<String>,
+    pub done: bool,
 }
 
 #[fn_handler]
 async fn hello_world() -> &'static str {
-    // RB.exec("CREATE TABLE biz_uuid( id VARCHAR, name VARCHAR, PRIMARY KEY(id));",vec![]).await;
-    RB.exec("SELECT NOW();", vec![]).await.unwrap();
+    RB.exec("CREATE TABLE `tab_todo` (`id` int unsigned NOT NULL AUTO_INCREMENT,`body` varchar(255) DEFAULT NULL,PRIMARY KEY (`id`));",vec![])
+    .await.unwrap();
     "Hello world"
 }
 
 #[fn_handler]
 async fn lists(res: &mut Response) {
     let result: Vec<TabTodo> = RB.fetch_list().await.unwrap();
-    // res.render(Text::Json(result));
+    res.render(Text::Json(serde_json::to_string(&result).unwrap()));
+}
+
+#[fn_handler]
+async fn update(res: &mut Response) {
+    let result: Vec<TabTodo> = RB.fetch_list().await.unwrap();
+    res.render(Text::Json(serde_json::to_string(&result).unwrap()));
 }
 
 #[tokio::main]
@@ -43,18 +50,7 @@ pub async fn main() {
         .unwrap();
     log::info!("linking database successful!");
     Server::new(TcpListener::bind("127.0.0.1:7878"))
-        .serve(Router::new().get(hello_world))
+        .serve(Router::new().get(lists).post(update))
         .await;
     log::info!("server quit!");
 }
-
-// pub fn set_json_response<T: serde::Serialize>(res: &mut Response, size: usize, json: T) {
-//     res.headers_mut().insert(
-//         header::CONTENT_TYPE,
-//         header::HeaderValue::from_static("application/json; charset=utf-8"),
-//     );
-//     let mut cache = Cache::with_capacity(size);
-//     serde_json::to_writer(&mut cache, &json).unwrap();
-//     res.set_body(Some(Body::Bytes(cache.into_inner())));
-//     res.set_status_code(StatusCode::OK)
-// }
